681dd31b45be31eac3572fc2415afac737cc89ce
chethan2010 <hemanthkumarsaketi@gmail.com>
2025-07-25 06:45:40 +0530

    jenkins-script

--- main.tf
+++ main.tf
@@ -69,17 +69,17 @@ module "records" {
       records = [
         module.jenkins-agent.private_ip
       ]
-    },
-     {
-      name    = "nexus"
-      type    = "A"
-      ttl     = 1
-      allow_overwrite = true
-      records = [
-        module.nexus.private_ip
-      ]
-      allow_overwrite = true
     }
+    #  {
+    #   name    = "nexus"
+    #   type    = "A"
+    #   ttl     = 1
+    #   allow_overwrite = true
+    #   records = [
+    #     module.nexus.private_ip
+    #   ]
+    #   allow_overwrite = true
+    # }
 
   ]
 }
