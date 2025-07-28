1645fec50f517012c5b76bbb2137fe9233085e59
chethan2010 <hemanthkumarsaketi@gmail.com>
2025-07-28 14:21:48 +0530

    expense-jenkins

--- jenkins.sh
+++ jenkins.sh
@@ -5,3 +5,7 @@ sudo yum install fontconfig java-21-openjdk
 systemctl daemon-reload
 systemctl enable jenkins
 systemctl start jenkins
+mkdir -p /var/lib/jenkins/.ssh
+echo -e "Host *\n\tStrictHostKeyChecking no\n\tUserKnownHostsFile=/dev/null" > /var/lib/jenkins/.ssh/config
+chown -R ec2-user:ec2-user /var/lib/jenkins/.ssh
+chmod 600 /var/lib/jenkins/.ssh/config
--- main.tf
+++ main.tf
@@ -1,9 +1,18 @@
+resource "aws_key_pair" "vpn" {
+  key_name   = "jenkins"
+  # you can paste the public key directly like this
+  public_key = "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIAVOVEfSZEvhDHtqiaeuuKOAb1O3ZfoO2IOp8ux/zCEl hp@DESKTOP-CVSHAG8"
+  # public_key = file("~/.ssh/openvpn.pub")
+  # ~ means windows home directory
+}
+
 module "jenkins" {
   source  = "terraform-aws-modules/ec2-instance/aws"
+  key_name = aws_key_pair.vpn.public_key
   name = "jenkins-tf"
   instance_type          = "t3.micro"
-  vpc_security_group_ids = ["sg-08f131185f8967295"]
-  subnet_id = "subnet-0f1043db64da18241"
+  vpc_security_group_ids = ["sg-02b46bf3da815a952"]
+  subnet_id = "subnet-04093fa40f500aed6"
   ami = data.aws_ami.ami_info.id
   user_data = file("jenkins.sh")
   tags = {
@@ -12,41 +21,15 @@ module "jenkins" {
 }
 }
 
-# resource "aws_security_group" "allow_tls" {
-#   # name        = "allow jenkins"
-#   # description = "jenkins_port"
-#   # vpc_id      = var.vpc_id
-#     ingress {
-#         from_port        = 8080
-#         to_port          = 8080
-#         protocol         = "tcp"
-#         cidr_blocks      = ["0.0.0.0/0"]
-#     }
-
-#     egress {
-#         from_port        = 0 # from 0 to 0 means, opening all protocols
-#         to_port          = 0
-#         protocol         = "-1" # -1 all protocols
-#         cidr_blocks      = ["0.0.0.0/0"]
-#     }
-
-#        tags = {
-#         Name = "allow_jenkins"
-#         CreatedBy = "Hemanth"
-#     }
-
-# }
-
-
-
 
 module "jenkins-agent" {
   source  = "terraform-aws-modules/ec2-instance/aws"
+  key_name = aws_key_pair.vpn.public_key
   name = "jenkins-tf"
 
   instance_type          = "t3.micro"
-  vpc_security_group_ids = ["sg-08f131185f8967295"]
-  subnet_id = "subnet-0f1043db64da18241"
+  vpc_security_group_ids = ["sg-02b46bf3da815a952"]
+  subnet_id = "subnet-04093fa40f500aed6"
   ami = data.aws_ami.ami_info.id
   user_data = file("jenkins-agent.sh")
   tags = {
@@ -111,3 +94,30 @@ module "records" {
 
   ]
 }
+
+
+
+# resource "aws_security_group" "allow_tls" {
+#   # name        = "allow jenkins"
+#   # description = "jenkins_port"
+#   # vpc_id      = var.vpc_id
+#     ingress {
+#         from_port        = 8080
+#         to_port          = 8080
+#         protocol         = "tcp"
+#         cidr_blocks      = ["0.0.0.0/0"]
+#     }
+
+#     egress {
+#         from_port        = 0 # from 0 to 0 means, opening all protocols
+#         to_port          = 0
+#         protocol         = "-1" # -1 all protocols
+#         cidr_blocks      = ["0.0.0.0/0"]
+#     }
+
+#        tags = {
+#         Name = "allow_jenkins"
+#         CreatedBy = "Hemanth"
+#     }
+
+# }
